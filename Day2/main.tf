fd57d116b1d43155d25c33b1f11f8374f1e06840
iam-amolgosavi <149919930+iam-amolgosavi@users.noreply.github.com>
2025-04-15 12:05:29 +0530

    Update main.tf

--- Day2/main.tf
+++ Day2/main.tf
@@ -1,1 +1,11 @@
+resource "aws_instance" "mera-instance" {
+      ami="ami-084568db4383264d4"
+      instance_type= "t2.micro"
+      tags={
+         Name="Terra-Thala"
+}
+}
 
+resource "aws_s3_bucket" "remote-bucket"{
+     bucket= "thala-for-reason-bucket"
+}
